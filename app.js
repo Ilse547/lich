30c99d694ebc22cce9c038cd00cfc46327b49177
lihuel <lihuel@debian>
2025-09-10 16:43:17 +0200

    Add static HTML files and CSS styling

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const { logger } = require('./middleware/logger');
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
-
-
-
-app.use(logger)
-
-app.get('/', (req, res) => {
-    res.send('hi')
-})
+app.use(logger);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`server working: http://localhost:${port}`)

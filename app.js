42b3435f4fdae1e6aaa243da0bcaad97761b055b
lihuel <lihuel@debian>
2025-10-01 22:04:14 +0200

    hi

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const path = require('path');
 const { logger } = require('./middleware/logger');
-const GUN = require('gun')
+const GUN = require('gun');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 const server = require('http').createServer(app);
-
 
 const gun = GUN({
     web: server,
@@ -21,8 +19,7 @@ const gun = GUN({
     ]
 });
 
-
-server.listen(port, ()=>{
-    console.log(`server working: http://localhost:${port}`);
-    console.log('gun relay peer run prt', port);
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    console.log('GUN relay peer running on port', port);
 });
